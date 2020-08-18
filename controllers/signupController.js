6fc18b0aec81d98b9aa71dcf43706c3c92962709
JakeW <jakewellsd@gmail.com>
2020-08-18 21:49:09 +0100

    add automatic login after signup

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -49,7 +49,10 @@ exports.user_create_post = [
         });
         user.save(function(err){
           if(err){return next(err);}
-          return res.redirect('/');
+          req.login(user, function(err){
+            if(err){return next(err);}
+            return res.redirect('/')
+          })
         })
       });
     }
