4c5fc1841fd76f80ce3b1d93803f3d2885e7bf22
JakeW <jakewellsd@gmail.com>
2020-08-20 22:21:48 +0100

    fix checking of spaces in subreddit names

--- routes/subreddit.js
+++ routes/subreddit.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var subredditController = require('../controllers/subredditController');
 
-/* GET home page. */
 router.get('/create', subredditController.subreddit_create_get);
 
 router.post('/create', subredditController.subreddit_create_post);
