96c57dbafbddf2cee1363c99337c4c09c3996558
JakeW <jakewellsd@gmail.com>
2020-10-01 21:03:03 +0100

    move index route to seperate controller

---
+++ controllers/frontPageController.js
@@ -0,0 +1,41 @@
+const Post = require("../models/post");
+const PostUpvote = require("../models/votes/post_upvote");
+const PostDownvote = require("../models/votes/post_downvote");
+const async = require("async");
+
+const getUserVotes = require('../public/javascripts/getUserVotes.js');
+
+exports.default_view_get =  function(req, res, next) {
+  async.parallel({
+    posts: function(callback){
+      Post.find()
+      .sort({date_created_at:-1, upvote_count :1})
+      .populate('subForum')
+      .populate('submitter')
+      .exec(callback);
+    },
+    post_upvotes: function(callback){
+      if(res.locals.currentUser){
+        PostUpvote.find({submitter: res.locals.currentUser._id})
+        .exec(callback);
+      }
+      else{ callback(null,[])}
+    },
+    post_downvotes: function(callback){
+      if(res.locals.currentUser){
+        PostDownvote.find({submitter: res.locals.currentUser._id})
+        .exec(callback);
+      }
+      else{ callback(null,[])}
+    }
+  }, function(err,results){
+    if(err){return next(err);}
+    let posts = results.posts
+    if(res.locals.currentUser){
+      posts = getUserVotes(results.posts, results.post_upvotes, results.post_downvotes);
+    }
+    res.render('index', { title: 'Forum Project', posts:posts});
+  })
+  
+}
+
--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const Post = require("../models/post");
-const PostUpvote = require("../models/votes/post_upvote");
-const PostDownvote = require("../models/votes/post_downvote");
-const async = require("async");
 
-const getUserVotes = require('../public/javascripts/getUserVotes.js');
+var frontPageController = require('../controllers/frontPageController');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  async.parallel({
-    posts: function(callback){
-      Post.find()
-      .sort({date_created_at:-1, upvote_count :1})
-      .populate('subForum')
-      .populate('submitter')
-      .exec(callback);
-    },
-    post_upvotes: function(callback){
-      if(res.locals.currentUser){
-        PostUpvote.find({submitter: res.locals.currentUser._id})
-        .exec(callback);
-      }
-      else{ callback(null,[])}
-    },
-    post_downvotes: function(callback){
-      if(res.locals.currentUser){
-        PostDownvote.find({submitter: res.locals.currentUser._id})
-        .exec(callback);
-      }
-      else{ callback(null,[])}
-    }
-  }, function(err,results){
-    if(err){return next(err);}
-    let posts = results.posts
-    if(res.locals.currentUser){
-      posts = getUserVotes(results.posts, results.post_upvotes, results.post_downvotes);
-    }
-    res.render('index', { title: 'Forum Project', posts:posts});
-  })
-  
-});
+router.get('/', frontPageController.default_view_get);
 
 module.exports = router;
