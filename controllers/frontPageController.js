91462e84a39dd8c04d9fe5bc9d9fe0ab71f2e13b
JakeW <jakewellsd@gmail.com>
2020-10-05 09:53:22 +0100

    add route to show top upvoted posts of all time

--- controllers/frontPageController.js
+++ controllers/frontPageController.js
@@ -72,3 +72,35 @@ exports.new_get = function(req,res,next){
   })
 }
 
+exports.top_get = function(req,res,next){
+  async.parallel({
+    posts: function(callback){
+      Post.find()
+      .sort({upvote_count:-1})
+      .populate('subForum')
+      .populate('submitter')
+      .exec(callback);
+    },
+    post_upvotes: function(callback){
+      if(res.locals.currentUser){
+        PostUpvote.find({submitter: res.locals.currentUser._id})
+        .exec(callback);
+      }
+      else{ callback(null,[])}
+    },
+    post_downvotes: function(callback){
+      if(res.locals.currentUser){
+        PostDownvote.find({submitter: res.locals.currentUser._id})
+        .exec(callback);
+      }
+      else{ callback(null,[])}
+    }
+  }, function(err,results){
+    if(err){return next(err);}
+    let posts = results.posts
+    if(res.locals.currentUser){
+      posts = getUserVotes(results.posts, results.post_upvotes, results.post_downvotes);
+    }
+    res.render('index', { title: 'Forum Project', posts:posts});
+  })
+}
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,8 @@ var router = express.Router();
 
 var frontPageController = require('../controllers/frontPageController');
 
-/* GET home page. */
 router.get('/', frontPageController.default_view_get);
-
 router.get('/new',frontPageController.new_get);
+router.get('/top',frontPageController.top_get);
 
 module.exports = router;
