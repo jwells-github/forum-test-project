d4a90a62eb9e9c51dd0b697f84dba06ea1b0fddb
JakeW <jakewellsd@gmail.com>
2020-10-05 21:03:42 +0100

    change the order that posts are presented for the default view

--- controllers/frontPageController.js
+++ controllers/frontPageController.js
@@ -6,10 +6,42 @@ const async = require("async");
 const getUserVotes = require('../public/javascripts/getUserVotes.js');
 
 exports.default_view_get =  function(req, res, next) {
+  let now = new Date(Date.now())
+  let one_day_ago = new Date()
+  one_day_ago.setHours(now.getHours()-24)
+  let two_days_ago = new Date()
+  two_days_ago.setHours(now.getHours()-(24*2))
+  let one_week_ago = new Date();
+  one_week_ago.setHours(now.getHours()-(24*7));
+  
   async.parallel({
-    posts: function(callback){
-      Post.find()
-      .sort({upvote_count :1})
+    todays_posts: function(callback){
+      Post.find({date_created_at: {$gte: one_day_ago}})
+      .sort({upvote_count :-1})
+      .populate('subForum')
+      .populate('submitter')
+      .limit(50)
+      .exec(callback);
+    },
+    yesterdays_posts: function(callback){
+      Post.find({date_created_at: {$gte: two_days_ago, $lt: one_day_ago}})
+      .sort({upvote_count :-1})
+      .populate('subForum')
+      .populate('submitter')
+      .limit(50)
+      .exec(callback);
+    },
+    last_weeks_posts: function(callback){
+      Post.find({date_created_at: {$gte: one_week_ago, $lt: two_days_ago}})
+      .sort({upvote_count :-1})
+      .populate('subForum')
+      .populate('submitter')
+      .limit(50)
+      .exec(callback);
+    },
+    older_posts: function(callback){
+      Post.find({date_created_at: {$lt: one_week_ago}})
+      .sort({upvote_count :-1})
       .populate('subForum')
       .populate('submitter')
       .limit(50)
@@ -31,11 +63,11 @@ exports.default_view_get =  function(req, res, next) {
     }
   }, function(err,results){
     if(err){return next(err);}
-    let posts = results.posts
+    let posts = results.todays_posts.concat(results.yesterdays_posts,results.last_weeks_posts,results.older_posts);
     if(res.locals.currentUser){
-      posts = getUserVotes(results.posts, results.post_upvotes, results.post_downvotes);
+      posts = getUserVotes(posts, results.post_upvotes, results.post_downvotes);
     }
-    res.render('index', { title: 'Forum Project', posts:posts});
+    res.render('index', { title: 'Forum Project', posts:posts, flash_messages: req.flash('info')});
   })
 }
 
